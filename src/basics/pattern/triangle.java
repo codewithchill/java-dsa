f7a78f2745929050d6da1590db2a74426f23bb2b
Swarnadeep Paul <201346855+codewithchill@users.noreply.github.com>
2025-11-24 23:32:00 +0530

    Update the triangle file with 5th pattern

--- src/basics/pattern/triangle.java
+++ src/basics/pattern/triangle.java
@@ -39,6 +39,12 @@ public class triangle {
       }
       System.out.println();
 
+      for (int i = 1; i <= num; i++) {
+        for (int j = i; j <= num; j++)
+          System.out.print(j + " ");
+        System.out.println();
+      }
+      System.out.println();
     }
   }
 }
