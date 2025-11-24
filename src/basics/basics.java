6794017e46eec5b24187cc0b61b8b5173490ae6d
Swarnadeep Paul <201346855+codewithchill@users.noreply.github.com>
2025-11-24 23:19:44 +0530

    Add the basic file

---
+++ src/basics/basics.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class basics {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.print("I'm \033[32mSwarnadeep\033[0m\n");
+    System.out.println(654.321);
+  }
+}
