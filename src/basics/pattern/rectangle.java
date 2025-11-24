374e0f978b1487bdbe7702ee4831cd3522920a50
Swarnadeep Paul <201346855+codewithchill@users.noreply.github.com>
2025-11-24 23:58:28 +0530

    Update the rectangle file

--- src/basics/pattern/rectangle.java
+++ src/basics/pattern/rectangle.java
@@ -9,13 +9,30 @@ public class rectangle {
       int rows = sc.nextInt();
       System.out.print("Enter the number of columns: ");
       int columns = sc.nextInt();
+
+      System.out.println();
       for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
+        for (int j = 0; j < columns; j++)
           System.out.print("* ");
+        System.out.println();
+      }
+      System.out.println();
+
+      for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= columns; j++) {
+          if (i == 1 || i == rows)
+            System.out.print("* ");
+          else {
+            if (j == 1 || j == columns) {
+              System.out.print("* ");
+            } else {
+              System.out.print("  ");
+            }
+          }
         }
         System.out.println();
       }
-
+      System.out.println();
     }
   }
 }
