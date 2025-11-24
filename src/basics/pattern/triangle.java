2dc1d6719d6a3400788b15896bb08b0a7033cc5b
Swarnadeep Paul <201346855+codewithchill@users.noreply.github.com>
2025-11-24 23:31:06 +0530

    Update the triangle file with 2nd pattern

--- src/basics/pattern/triangle.java
+++ src/basics/pattern/triangle.java
@@ -14,6 +14,13 @@ public class triangle {
         System.out.println();
       }
       System.out.println();
+
+      for (int i = 1; i <= num; i++) {
+        for (int j = 1; j <= i; j++)
+          System.out.print(j + " ");
+        System.out.println();
+      }
+      System.out.println();
     }
   }
 }
