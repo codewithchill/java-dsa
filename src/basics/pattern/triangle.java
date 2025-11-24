166710c062200c7e81dec867ff7804018f821edd
Swarnadeep Paul <201346855+codewithchill@users.noreply.github.com>
2025-11-24 23:31:50 +0530

    Update the triangle file with 4th pattern

--- src/basics/pattern/triangle.java
+++ src/basics/pattern/triangle.java
@@ -29,6 +29,16 @@ public class triangle {
       }
       System.out.println();
 
+      for (int i = 1; i <= num; i++) {
+        for (int j = 1; j <= i; j++)
+          if (i % 2 == 0)
+            System.out.print((char)(j + 'A' - 1) + " ");
+          else
+            System.out.print(j + " ");
+        System.out.println();
+      }
+      System.out.println();
+
     }
   }
 }
