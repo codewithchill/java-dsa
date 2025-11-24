a7227f12cf2498d27ab4eda49eec6a58325a6281
Swarnadeep Paul <201346855+codewithchill@users.noreply.github.com>
2025-11-24 23:24:02 +0530

    Add the rectangle file

---
+++ src/basics/pattern/rectangle.java
@@ -0,0 +1,21 @@
+package basics.pattern;
+
+import java.util.Scanner;
+
+public class rectangle {
+  public static void main(String[] args) {
+    try (Scanner sc = new Scanner(System.in)) {
+      System.out.print("Enter the number of rows: ");
+      int rows = sc.nextInt();
+      System.out.print("Enter the number of columns: ");
+      int columns = sc.nextInt();
+      for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+          System.out.print("* ");
+        }
+        System.out.println();
+      }
+
+    }
+  }
+}
