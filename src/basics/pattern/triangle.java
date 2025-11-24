bc5f1bfb539eab354fff205703ad8f7f2ea6828e
Swarnadeep Paul <201346855+codewithchill@users.noreply.github.com>
2025-11-25 00:01:53 +0530

    Update the triangle file

--- src/basics/pattern/triangle.java
+++ src/basics/pattern/triangle.java
@@ -41,14 +41,21 @@ public class triangle {
 
       for (int i = 1; i <= num; i++) {
         for (int j = i; j <= num; j++) 
-        /* 
-        * [j = i => j <= num] can be mathematically
-        * written as [j = 1 => j <= num - i + 1]
-        * j = i => j <= num
-        * j = 0 => j <= num - i
-        * j = 1 => j <= num - i + 1
-        */
           System.out.print(j + " ");
+        System.out.println();
+      }
+      System.out.println();
+
+      for (int i = 1; i <= num; i++) {
+        for (int j = 1; j <= num - i + 1; j++)
+          System.out.print((char)(i + 'A' - 1) + " ");
+        System.out.println();
+      }
+      System.out.println();
+
+      for (int i = 1; i <= num; i++) {
+        for (int j = 1; j <= num - i + 1; j++)
+          System.out.print((char)(j + 'A' - 1) + " ");
         System.out.println();
       }
       System.out.println();
